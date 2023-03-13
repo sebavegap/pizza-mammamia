e535fe1af3e058696906b6df512e0702b428a019
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-03-13 16:44:55 -0300

    card order

--- src/components/CardMini.jsx
+++ src/components/CardMini.jsx
@@ -9,41 +9,51 @@ import MyContext from '../Context'
 
 const CardMini = (pizza) => {
 
- 
 
-  return (
-    <div>
-        {/* se mapea el array de pizzas.js desde el estado y se genera una card por cada elemento */}
-        <Col xl={3} md={4} s={6} xs={6} className="p-1">
-        <Container className="py-2 h-100">
-            {/* se mapea el array de inventario.js desde el estado y se genera una card por cada elemento */}
-            
-                <Card style={{ width: "auto" }} className='mb-3 h-100' key={pizza.id}>
-                    <Card.Img variant="top" style={{height: "300px"}} src={pizza.imagen} />
-                    <Card.Body>
-                        <Card.Title>{pizza.nombre}</Card.Title>
-                        <Card.Text>
-                            {pizza.ingredientes}
-                            
-                            <br />
-                            {/*   formateo de figura.precio a moneda chilena con Intl.NumberFormat */}
-                            Precio:{" "}
-                             {new Intl.NumberFormat("es-CL", {
-                                style: "currency",
-                                currency: "CLP"
-                            })
-                                .format(pizza.precio
-                                )}
 
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            
-        </Container>
-        </Col>
-        
-    </div>
-  )
+    return (
+        <div>
+            {/* se mapea el array de pizzas.js desde el estado y se genera una card por cada elemento */}
+            <Col xl={3} md={4} s={6} xs={6} className="p-1">
+                <Container className="py-2 h-100">
+                    {/* se mapea el array de inventario.js desde el estado y se genera una card por cada elemento */}
+
+                    <Card style={{ width: "auto" }} className='mb-3 h-100' key={pizza.id}>
+                        <Card.Img variant="top" style={{ height: "300px" }} src={pizza.imagen} />
+                        <Card.Body>
+                            <Card.Title>
+                                {/* parse del nombre para añadir mayuscula */}
+                                {pizza.nombre.charAt(0).toUpperCase() + pizza.nombre.slice(1)}
+                            </Card.Title>
+                            <Card.Text>
+                                <ul>
+                                    <li>{pizza.ingredientes[0]}</li>
+                                    <li>{pizza.ingredientes[1]}</li>
+                                    <li>{pizza.ingredientes[2]}</li>
+                                    <li>{pizza.ingredientes[3]}</li>
+                                </ul>
+
+
+
+                                <br />
+                                {/*   formateo de figura.precio a moneda chilena con Intl.NumberFormat */}
+                                Precio:{" "}
+                                {new Intl.NumberFormat("es-CL", {
+                                    style: "currency",
+                                    currency: "CLP"
+                                })
+                                    .format(pizza.precio
+                                    )}
+
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                </Container>
+            </Col>
+
+        </div>
+    )
 }
 
 export default CardMini
