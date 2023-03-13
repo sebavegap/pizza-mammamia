d57b76d48d6f64affee6b1eaa3fbb41f8d1b285f
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-03-13 17:17:45 -0300

    debug carrito

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-//import de context
-import MyContext from '../Context'
-//import de hooks
-import { useContext } from 'react'
+
 
 const ListaProductos = () => {
 
-  const { datosCarrito, setdatosCarrito } = useContext(MyContext)
-  console.log(datosCarrito)
+
 
   return (
     <div>ListaProductos</div>
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 // import de componentes
 import ListaProductos from '../components/ListaProductos'
+//import de context
+import MyContext from '../Context'
+//import de hooks
+import { useContext } from 'react'
 
 
 const Carrito = () => {
+
+  const { datosCarrito, setdatosCarrito } = useContext(MyContext)
+  console.log(datosCarrito)
   return (
     <div>
       <h1>Carrito</h1>
