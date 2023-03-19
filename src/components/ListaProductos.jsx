0e638d18c8916b77f38b05681ee2a6ef5aa25ae1
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-03-19 19:38:01 -0300

    mensaje lista

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+//import del contexto
+import MyContext from '../Context'
+
+//import de los hooks
+import { useContext } from 'react'
 
 
 const ListaProductos = () => {
 
+  //consumimos el contexto
+  const { datosCarrito, setdatosCarrito } = useContext(MyContext)
+
+// si no hay productos en el carrito, mostrar mensaje
+if (datosCarrito.length === 0) {
+  return (
+    <div>
+      <h3>No hay productos en el carrito</h3>
+    </div>
+  )
+}
 
 
   return (
     <div>ListaProductos</div>
+    /* retornamos una lista con los productos en datosCarrito */
+
+
   )
 }
 
