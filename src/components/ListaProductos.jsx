e6307113e8e6108727002092a26805f148bbe6bc
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-03-21 22:19:57 -0300

    sumar y restar pizzas carrito

--- src/components/ListaProductos.jsx
+++ src/components/ListaProductos.jsx
@@ -14,6 +14,49 @@ const ListaProductos = () => {
   //consumimos el contexto
   const { datosCarrito, setdatosCarrito, precioTotal, setprecioTotal } = useContext(MyContext)
 
+//función que reduce en 1 la cantidad de la pizza segun el id, y la elimina si la cantidad es 0
+const reducirPizza = (id) => {
+  //se hace un mapeo de los productos en el carrito
+  const nuevoCarrito = datosCarrito.map((producto) => {
+    //si el id del producto es igual al id del producto que se quiere eliminar, se reduce en 1 la cantidad
+    if (producto.id === id) {
+      return {
+        ...producto,
+        //se reduce en 1 la cantidad de la pizza
+        quantity: producto.quantity - 1
+      }
+    }
+    //si el id no es el mismo, se retorna el producto sin cambios, asi evitamos que se cambien las otras pizzas
+    else {
+      return producto
+    }
+  })
+  //se filtran las pizzas que tengan una cantidad mayor a 0
+  const filteredCart = nuevoCarrito.filter((producto) => producto.quantity > 0)
+  //se actualiza el estado de datosCarrito, esto va a lograr actualizar el carrito y por ende el precio total de Navigation
+  setdatosCarrito(filteredCart)
+}
+
+//Ahora creamos una función que pueda sumar 1 pizza desde la ventana del carrito, la logica es la misma que para reducir
+const aumentarPizza = (id) => {
+  //se hace un mapeo de los productos en el carrito
+  const nuevoCarrito = datosCarrito.map((producto) => {
+    //si el id del producto es igual al id del producto que se quiere eliminar, se reduce en 1 la cantidad
+    if (producto.id === id) {
+      return {
+        ...producto,
+        //se reduce en 1 la cantidad de la pizza
+        quantity: producto.quantity + 1
+      }
+    }
+    //si el id no es el mismo, se retorna el producto sin cambios, asi evitamos que se cambien las otras pizzas
+    else {
+      return producto
+    }
+  })
+  //se actualiza el estado de datosCarrito, esto va a lograr actualizar el carrito y por ende el precio total de Navigation
+  setdatosCarrito(nuevoCarrito)
+}
 
 
 
@@ -38,7 +81,10 @@ else {
       {datosCarrito.map((producto) => (
 
         <ListGroupItem key={producto.id}>
-          Pizza {producto.name} - Precio Unitario: {producto.price} - Cantidad: {producto.quantity} - Subtotal: {producto.price * producto.quantity}
+          Pizza {producto.name} - Precio Unitario: {producto.price} - Cantidad:
+          <button onClick={() => reducirPizza(producto.id)}>-</button>
+           {producto.quantity}
+           <button onClick={() => aumentarPizza(producto.id)}>+</button> - Subtotal: {producto.price * producto.quantity}
         </ListGroupItem>
       ))}
       <ListGroupItem>
